cc027b7112ea1ed9b85449843c41bf8d951f41c5
Lukas Kratochvil <lkratochvil57@gmail.com>
2025-11-22 21:58:03 +0100

    feat (api): upgraded ValidationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,13 +9,21 @@ import type { ConfigSchema } from "./config/schema";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = app.get(ConfigService<ConfigSchema, true>);
-
-  // Validation of HTTP requests
-  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
+  const nodeEnv = config.get("nodeEnv", { infer: true });
 
   // Setup logger
   const logger = createWinstonLogger("API");
   app.useLogger(logger);
+
+  // Validation of HTTP requests
+  app.useGlobalPipes(
+    // See: https://docs.nestjs.com/techniques/validation#using-the-built-in-validationpipe
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      transform: true, // enables class-transformer transformations (like `@Type()`)
+      enableDebugMessages: nodeEnv === "development",
+    })
+  );
 
   // Starting the app
   const port = config.get("port", { infer: true });
