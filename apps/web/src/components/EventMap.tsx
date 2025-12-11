0a426201a49096196bf1f7be657ae22016d09415
Lukas Kratochvil <lkratochvil57@gmail.com>
2025-12-11 18:35:11 +0100

    feat (web): added map

--- apps/web/src/components/EventMap.tsx
+++ apps/web/src/components/EventMap.tsx
@@ -1,6 +1,25 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 const EventMap = () => {
   // TODO: create component
-  return <></>;
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ width: 500, height: 500 }} // size must be specified to display the map in the UI
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default EventMap;
