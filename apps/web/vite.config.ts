ff4c6fda47216ccb397d90f10466d8b19a4d9658
Lukas Kratochvil <lkratochvil57@gmail.com>
2026-03-10 20:07:28 +0100

    style: code format and lint

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const port = process.env["PORT"] ? +process.env["PORT"] : undefined;
 
