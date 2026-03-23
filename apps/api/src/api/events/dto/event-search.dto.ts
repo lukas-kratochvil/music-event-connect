0a5e14efda6b4bd4996bdcd6d77d3fd910e58d35
Lukas Kratochvil <lkratochvil57@gmail.com>
2026-03-23 18:38:07 +0100

    feat: validate events search filters

--- apps/api/src/api/events/dto/event-search.dto.ts
+++ apps/api/src/api/events/dto/event-search.dto.ts
@@ -1,12 +1,24 @@
+import { IsDateMoreInFutureThan } from "@music-event-connect/core/validation";
 import { ApiPropertyOptional } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { ArrayUnique, IsArray, IsDate, IsOptional } from "class-validator";
 
 export class EventsSearchDTO {
   @ApiPropertyOptional({ type: "string", isArray: true, required: false })
+  @IsOptional()
+  @IsArray()
+  @ArrayUnique<string>()
   artistNames?: string[];
 
   @ApiPropertyOptional({ type: Date, required: false })
+  @Type(() => Date)
+  @IsOptional()
+  @IsDate()
   startFrom?: Date;
 
   @ApiPropertyOptional({ type: Date, required: false })
+  @Type(() => Date)
+  @IsOptional()
+  @IsDateMoreInFutureThan<EventsSearchDTO>("startFrom")
   startTo?: Date;
 }
