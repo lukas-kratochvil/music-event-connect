dcb63947bba7d9289c1ef3d7b21289aa1bdb7157
Lukas Kratochvil <lkratochvil57@gmail.com>
2026-04-15 18:09:27 +0200

    docs: edit or remove comments

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -22,7 +22,6 @@ import { configSchema, type ConfigSchema } from "./config/schema";
       ],
     }),
     // TODO: caching with Redis (https://docs.nestjs.com/techniques/caching)
-    // TODO: REST API documentation with Swagger (https://docs.nestjs.com/openapi/introduction)
     EventsModule,
   ],
   providers: [
