4358bf23fcacf77cf29a7e70d9a7c7b9d32d6cd4
Lukas Kratochvil <lkratochvil57@gmail.com>
2026-04-24 12:28:18 +0200

    refactor(scraper): move config loading in the AppModule

--- apps/scraper/src/app.module.ts
+++ apps/scraper/src/app.module.ts
@@ -1,17 +1,20 @@
+import { loadYamlConfig } from "@music-event-connect/core/config";
 import { Logger, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
-import { appConfig } from "./config/app-config";
+import { configSchema } from "./config/schema";
 import { CronManagerModule } from "./cron/cron-manager.module";
 import { QueueModule } from "./queue/queue.module";
+
+const config = loadYamlConfig("config.yaml", configSchema);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [() => appConfig],
+      load: [() => config],
     }),
     ScheduleModule.forRoot(),
-    CronManagerModule.register(appConfig),
+    CronManagerModule.register(config),
     QueueModule,
   ],
   providers: [Logger],
--- apps/scraper/src/config/app-config.ts
+++
@@ -1,4 +0,0 @@
-import { loadYamlConfig } from "@music-event-connect/core/config";
-import { configSchema } from "./schema";
-
-export const appConfig = loadYamlConfig("config.yaml", configSchema);
